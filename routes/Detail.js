135292921ed209ed6ac49762fe428f78fb0d8e22
naeunk1m <107620483+naeunk1m@users.noreply.github.com>
2023-03-01 20:06:06 +0900

    Add files via upload

---
+++ routes/Detail.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
+import styled from 'styled-components';
+import { Nav } from 'react-bootstrap';
+
+function Detail(props) {
+    let [Alert, setAlert] = useState(true);
+    let [count, setCount] = useState(0)
+    let { id } = useParams();
+    let [num, setNum] = useState('')
+    let 찾은상품 = props.shoes.find(function (x) {
+        return x.id == id
+    })
+    let [탭, 탭변경] = useState(0)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setAlert(false)
+        }, 2000);
+    }, [])
+
+    useEffect(() => {
+        if (isNaN(num) == true) {
+            alert('그러지마삼')
+        }
+    }, [num])
+
+    return (
+
+        <div className="container">
+
+            {Alert == true
+                ? <div className='alert alert-warning'>
+                    2초 이내 구매시 할인
+                </div> : null}
+
+            {count}
+            <button onClick={() => { setCount(count + 1) }}>버튼</button>
+            <br />
+            <input onChange={(e) => { setNum(e.target.value) }} />
+            <div className="row">
+                < div className="col-md-6" >
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                </div >
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">{찾은상품.title}</h4>
+                    <p>{찾은상품.content}</p>
+                    <p>{찾은상품.price}원</p>
+                    <button className="btn btn-danger">주문하기</button>
+                </div>
+            </div >
+            <Nav variant="tabs" defaultActiveKey="link0">
+
+                <Nav.Item>
+                    <Nav.Link onClick={() => { 탭변경(0) }} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={() => { 탭변경(1) }} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={() => { 탭변경(2) }} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent 탭={탭} />
+        </div >
+
+    )
+}
+
+
+function TabContent({탭}){
+
+    let [fade, setFade] = useState('')
+
+    useEffect(()=>{
+        setTimeout(() => {
+            setFade('end')
+        }, 100);
+        return ()=> {
+            setFade('')
+        }
+    }, [탭])
+    return (
+      <div className={'start ' + fade}>
+        { [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭] }
+      </div>
+    )
+  }
+
+export default Detail
